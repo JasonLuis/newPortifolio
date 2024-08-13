fbab3786f5883531b1f5137bac2f76a68e6f2016
Djeison Luis <jason20luis@gmail.com>
2024-08-12 23:47:33 -0300

    📦feat: cria componente ImagePerfil

---
+++ src/core/components/ImagePerfil.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="perfil">
+    <div class="row justify-end q-pt-xl q-pr-lg">
+      <div class="list-icons">
+        <q-icon :name="`img: ${icons.github.icon}`" />
+        <q-icon :name="`img: ${icons.frontMentor.icon}`" />
+        <q-icon :name="`img: ${icons.linkedin.icon}`" />
+        <q-icon :name="`img: ${icons.twitter.icon}`" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const icons = {
+  github: {
+    icon: new URL("../assets/icon/icon-github.svg", import.meta.url),
+    link: "#",
+  },
+  frontMentor: {
+    icon: new URL(
+      "../assets/icon/icon-frontend-mentor.svg",
+      import.meta.url
+    ),
+    link: "#",
+  },
+  linkedin: {
+    icon: new URL("../assets/icon/icon-linkedin.svg", import.meta.url),
+    link: "#",
+  },
+  twitter: {
+    icon: new URL("../assets/icon/icon-twitter.svg", import.meta.url),
+    link: "#",
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.perfil {
+  width: 445px;
+  height: 720px;
+  background-color: #242424;
+  background: url("../assets/icon/profile.png") lightgray 0px 0px / 100%
+    100% no-repeat;
+}
+
+.list-icons {
+  display: flex;
+  align-items: center;
+  min-width: 192.5px;
+  justify-content: space-between;
+}
+
+.q-icon {
+  height: 25px;
+  width: 25px;
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,14 +6,7 @@
         <UiBoxPerfil style="margin-top: 157px;"/>
       </div>          
       <div class="info-right">
-        <div class="row justify-end q-pt-xl q-pr-lg">
-          <div class="list-icons">
-            <q-icon :name="`img: ${icons.github.icon}`" />
-            <q-icon :name="`img: ${icons.frontMentor.icon}`" />
-            <q-icon :name="`img: ${icons.linkedin.icon}`" />
-            <q-icon :name="`img: ${icons.twitter.icon}`" />
-          </div>
-        </div>
+        <UiImageProfile />
       </div>
     </div>
   </section>
@@ -21,28 +14,8 @@
 
 <script setup lang="ts">
 import UiBoxPerfil from "~~/src/core/components/BoxPerfil.vue"; 
+import UiImageProfile from "~~/src/core/components/ImagePerfil.vue"
 
-const icons = {
-  github: {
-    icon: new URL("../core/assets/icon/icon-github.svg", import.meta.url),
-    link: "#",
-  },
-  frontMentor: {
-    icon: new URL(
-      "../core/assets/icon/icon-frontend-mentor.svg",
-      import.meta.url
-    ),
-    link: "#",
-  },
-  linkedin: {
-    icon: new URL("../core/assets/icon/icon-linkedin.svg", import.meta.url),
-    link: "#",
-  },
-  twitter: {
-    icon: new URL("../core/assets/icon/icon-twitter.svg", import.meta.url),
-    link: "#",
-  },
-};
 </script>
 
 <style lang="scss">
@@ -65,30 +38,13 @@ section {
   letter-spacing: -0.444px;
 }
 
-.list-icons {
-  display: flex;
-  align-items: center;
-  min-width: 192.5px;
-  justify-content: space-between;
-}
-
 .info-left {
   z-index: 2;
 }
 
 .info-right {
-  width: 445px;
-  height: 720px;
-  background-color: #242424;
-  background: url("../core/assets/icon/profile.png") lightgray 0px 0px / 100%
-    100% no-repeat;
   z-index: 1;  
   position: relative;
   left: -40px;
-}
-
-.q-icon {
-  height: 25px;
-  width: 25px;
 }
 </style>
